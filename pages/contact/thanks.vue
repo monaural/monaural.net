19f1b8a8b84530b79e6ed29961d0911362926452
h.matsuo <matsuo@monaural.net>
2018-10-14 11:23:58 +0900

    form test

---
+++ pages/contact/thanks.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <h3 class="title">Contact / お問合せ</h3>
+    <p>お問合せありがとうございました。通常3営業日以内に返信いたします。</p>
+    <p><br>
+      <nuxt-link to="/">トップに戻る</nuxt-link>
+    </p>
+  </main>
+</template>
