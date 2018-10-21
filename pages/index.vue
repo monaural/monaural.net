3f4a3c194f17f1966d2327fa65345facbede830f
matsuoshi <matsuo@monaural.net>
2018-10-22 07:19:42 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="home">
     <ul>
-      <li><a href="https://blog.monaural.net/">blog</a></li>
+      <li><a href="https://blog.monaural.net/">ブログ</a></li>
       <li><a href="https://pause.monaural.net/">pause // music blog</a></li>
-      <li><a href="https://matsuoshi.hatenablog.com/">はてな</a></li>
+      <li><a href="https://matsuoshi.hatenablog.com/">はてなブログ</a></li>
     </ul>
 
     <p>
